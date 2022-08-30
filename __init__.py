d5d62412c4f61a0f345ec0ce55df8ced52f31bb0
ching shin lu <chingshinlu2001@gmail.com>
2022-08-30 17:05:45 +0800

    add jinja

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from fastapi.templating import Jinja2Templates
+import os,shelve
+from datetime import datetime, timedelta,date
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello world!'
+
+
+BASEDIR = os.getcwd()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 templates = Jinja2Templates(directory=f"{BASEDIR}/templates")
 
