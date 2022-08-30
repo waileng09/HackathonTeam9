8a9d7f6bdaf44d76f383df4d068b37c1ef9a7f17
ching shin lu <chingshinlu2001@gmail.com>
2022-08-30 17:24:06 +0800

    no need rerun

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from fastapi.templating import Jinja2Templates
+import os, shelve
 
 app = Flask(__name__)
+BASEDIR = os.getcwd()
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+templates = Jinja2Templates(directory=f"{BASEDIR}/templates")
 
 @app.route('/')
 def home():
