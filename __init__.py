942cfcb5ddd7e960430ee99c9825da5f4c69ebf1
ngcaixian <214407N@mymail.nyp.edu.sg>
2022-08-30 17:25:57 +0800

    edited __init__.py (customer_home route)

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello world!'
+def customer_home():
+    return render_template('customer_home.html')
 
 if __name__ == '__main__':
     app.debug = True
