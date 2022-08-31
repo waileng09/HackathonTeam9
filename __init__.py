cae317eca29b2f2dccea7d2154edac204239881b
SHKXXX <89094668+SHKXXX@users.noreply.github.com>
2022-09-01 03:05:20 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from fastapi.templating import Jinja2Templates
+from Form import CreateCustomerForm
+import Customer
 from uuid import uuid4
 import os, shelve
 from Form import RecyclingForm
 from functions import Recycling
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from datetime import datetime, timedelta,date
 
 app = Flask(__name__)
 BASEDIR = os.getcwd()
@@ -92,6 +95,147 @@ def staff_dashboard():
 @app.route('/tree')
 def tree():
     return render_template('')
+
+#sign up page
+@app.route('/createCustomer', methods=['GET', 'POST'])
+def create_user():
+    #fix the counting
+    db = shelve.open('customer.db', 'r')
+    customers_dict = db['Customers']
+    if len(customers_dict) > 0:
+        x = list(reversed(list(customers_dict)))[0]
+        print(x)
+        total_count = x + 1
+    else:
+        total_count = 1
+    #total_count = len(customers_dict)+1
+    create_customer_form = CreateCustomerForm(request.form)
+    print(create_customer_form)
+    if request.method == 'POST':
+        customers_dict = {}
+        db = shelve.open('customer.db', 'c')
+        print('pass')
+        try:
+            customers_dict = db['Customers']
+        except:
+            print("Error in retrieving Customers from customer.db.")
+
+        if create_customer_form.password.data == create_customer_form.password2.data:
+            last_login = datetime.now()
+            status = True
+            customer = Customer.Customer(create_customer_form.first_name.data, create_customer_form.last_name.data,
+                                     create_customer_form.email_address.data,
+                                     create_customer_form.password.data, create_customer_form.password2.data, create_customer_form.type.data, create_customer_form.birthday.data)
+
+
+            session['logged_in'] = True
+            if customer.get_type() == 'Customer':
+                print('added')
+                customer.set_customer_id(total_count)
+                customers_dict[customer.get_customer_id()] = customer
+                db['Customers'] = customers_dict
+
+
+                #login file
+                login_file = open('login.txt', 'w')
+                email = customer.get_email_address()
+                birthday = customer.get_birthday()
+                login_name = customer.get_first_name() + ' ' + customer.get_last_name()
+                login_file.write(login_name + '\n')
+                login_file.write(email + '\n')
+                login_file.write(str(birthday))
+                login_file.close()
+
+                print('customer_info')
+
+
+        else:
+            print("password is different")
+            flash("password is different", category='error')
+            return render_template('customer page/create_acc.html', form=create_customer_form)
+        #fix
+        customer.set_customer_id(total_count)
+        customers_dict[customer.get_customer_id()] = customer
+        db['Customers'] = customers_dict
+
+        db.close()
+
+
+
+        print('retrieve')
+    return render_template('customer page/create_acc.html', form=create_customer_form)
+
+#login page
+@app.route('/CustomerLogin', methods=['GET', 'POST'])
+def user_login():
+    print('ok')
+    user_login_form = CreateCustomerForm(request.form)
+    if request.method == 'POST':
+        email = user_login_form.email_address.data
+        password = user_login_form.password.data
+        print('{} {}'.format(email, password))
+        # validation
+        customers_dict = {}
+        db = shelve.open('customer.db', 'r')
+        customers_dict = db['Customers']
+        customer = customers_dict.get(id)
+        for key in customers_dict:
+            if email == customers_dict[key].get_email_address():
+                if password == customers_dict[key].get_password():
+                    print("Login successfuly")
+                    session["customerID"] = key
+                    print(session["customerID"])
+                    session['logged_in'] = True
+                    # login file
+                    login_name = customers_dict[key].get_first_name() + ' ' + customers_dict[key].get_last_name()
+                    birthday = customers_dict[key].get_birthday()
+                    login_file = open('login.txt', 'w')
+                    login_file.write(login_name + '\n')
+                    login_file.write(email + '\n')
+                    login_file.write(str(birthday))
+                    login_file.close()
+
+                    date = birthday.strftime("%d")
+                    month = birthday.strftime("%m")
+                    print(birthday)
+                    print(date)
+                    print(month)
+
+                    if customers_dict[key].get_type() == 'Customer':
+                       return redirect(url_for('customer_info'))
+
+                    else:
+                        return redirect(url_for('staff_dashboard'))
+
+                else:
+                    print("Incorrect password")
+
+            else:
+                print("email does not exist")
+
+        db.close()
+    return render_template('customer page/customer_login.html', form=user_login_form)
+
+#customer info
+@app.route('/CustomerInfo')
+def customer_info():
+    login_file = open('login.txt', 'r')
+    name = login_file.readline()
+    email = login_file.readline()
+    birthday = login_file.readline()
+    print(name + email + birthday)
+    login_file.close()
+    return render_template('customer page/customer_info.html', name = name, email = email, birthday = birthday)
+
+#reward display(customer side)
+@app.route('/displayrewards')
+def display_rewards():
+    return render_template('customer page/customer_rewards.html')
+
+#logout
+@app.route('/logout')
+def logout():
+    return render_template('customer page/customer_home.html')
 
 
 if __name__ == '__main__':
